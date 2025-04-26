b59b4ffccdff1ab19a79a9596e476b718804b7e2
ثنا شیرزاد <1363045709@refahbank.net>
2025-04-26 09:22:21 +0330

    initialize project

---
+++ Data/ToDoList.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloToAsp.Data;
+
+public class ToDoList
+{
+    public int Id { get; set; }
+    public string Task { get; set; }
+    public string? StartDateTime { get; set; }
+    public string? EndDateTime { get; set; }
+    public int? Duration { get; set; }
+}
